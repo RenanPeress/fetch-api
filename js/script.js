fa36fff39f47fde4045317df207c5a6a5c4d6701
Renan Clemente Peres <134585166+RenanPeress@users.noreply.github.com>
2024-01-30 14:46:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ async function getAllPosts() {
         title.innerText = post.title;
         body.innerText = post.body;
         link.innerText = "Ler";
-        link.setAttribute('href', `renanperess.github.io/`);
+        link.setAttribute('href', `/post.html?id=${post.id}`);
 
         div.appendChild(title);
         div.appendChild(body);
